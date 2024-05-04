bf51a354dc614bdb6943e0a1f4009adebf0d8d34
asfour0406 <157413272+asfour0406@users.noreply.github.com>
2024-05-04 11:23:08 -0500

    learning java i feel really slow rn

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("welecome to the number guessing game!");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("you will be guessing a number from one to 100");
+            double randNumber = Math.random();
+            double d = randNumber * 100;   
+            int randomInt1 = (int)d;
+            System.out.println(randomInt1);
+            int counter = 0;
+            boolean game = true;
+       
+            while(game){
+                counter++;
+                System.out.println("guess: "+counter );
+                int guess = scanner.nextInt();
+                if(guess == randomInt1){
+                    System.out.println("You guessed correctly!");
+                }else if((randomInt1 +10 > guess && guess > randomInt1 -10 )){
+                    System.out.println("HOT");
+                }else{
+                    System.out.println("Cold");
+                }
+            }
+
+     }
+    }
+    
+
